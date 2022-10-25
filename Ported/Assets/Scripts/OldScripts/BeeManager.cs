ccdd1bc4ff2df06afa4230e94cfb28b5bf9d38da
Jonas Schartau Grooss <jonasgrooss@hotmail.com>
2022-10-25 16:32:29 +0200

    commented old code for future
    Added comments to a fair bit of the code to make it easier to remember what each part does later when we are converting it to ECS

--- Ported/Assets/Scripts/OldScripts/BeeManager.cs
+++ Ported/Assets/Scripts/OldScripts/BeeManager.cs
@@ -41,14 +41,18 @@ public class BeeManager : MonoBehaviour {
 	const int beesPerBatch=1023;
 	MaterialPropertyBlock matProps;
 
+	//Used when the position of the bees to spawn aren't known
 	public static void SpawnBee(int team) {
 		Vector3 pos = Vector3.right * (-Field.size.x * .4f + Field.size.x * .8f * team);
 		instance._SpawnBee(pos,team);
 	}
 
+	//Used when the position for the bees to spawn is known
 	public static void SpawnBee(Vector3 pos,int team) {
 		instance._SpawnBee(pos,team);
 	}
+
+	//The function that actually spawns the bees
 	void _SpawnBee(Vector3 pos, int team) {
 		Bee bee;
 		if (pooledBees.Count == 0) {
@@ -71,6 +75,8 @@ public class BeeManager : MonoBehaviour {
 		beeMatrices[activeBatch].Add(Matrix4x4.identity);
 		beeColors[activeBatch].Add(teamColors[team]);
 	}
+
+	//Deletes the bees from both the game and the team when needed
 	void DeleteBee(Bee bee) {
 		pooledBees.Add(bee);
 		bees.Remove(bee);
@@ -100,6 +106,7 @@ public class BeeManager : MonoBehaviour {
 		for (int i=0;i<2;i++) {
 			teamsOfBees[i] = new List<Bee>(25000);
 		}
+		//Spawns the initial bees
 		for (int i=0;i<startBeeCount;i++) {
 			int team = i%2;
 			SpawnBee(team);
@@ -112,22 +119,32 @@ public class BeeManager : MonoBehaviour {
 	void FixedUpdate() {
 		float deltaTime = Time.fixedDeltaTime;
 
+		//loop through every single bee in the game
 		for (int i = 0; i < bees.Count; i++) {
 			Bee bee = bees[i];
 			bee.isAttacking = false;
 			bee.isHoldingResource = false;
+			//Check if bee is not dead
 			if (bee.dead == false) {
+				//Set velocity to random direction
 				bee.velocity += Random.insideUnitSphere * (flightJitter * deltaTime);
 				bee.velocity *= (1f-damping);
 
+				//Find all allied bees
 				List<Bee> allies = teamsOfBees[bee.team];
+				//Pick random friendly bee
 				Bee attractiveFriend = allies[Random.Range(0,allies.Count)];
+				//calculate difference in position between the two bees
 				Vector3 delta = attractiveFriend.position - bee.position;
+				//Calculate distance
 				float dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+				//Update bee velocity if distance is greater than 0
 				if (dist > 0f) {
 					bee.velocity += delta * (teamAttraction * deltaTime / dist);
 				}
 
+				//Find another random friendly bee (why is this not used)
+				//Do same operations as above, think this is coded incorrectly
 				Bee repellentFriend = allies[Random.Range(0,allies.Count)];
 				delta = attractiveFriend.position - bee.position;
 				dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
@@ -135,27 +152,38 @@ public class BeeManager : MonoBehaviour {
 					bee.velocity -= delta * (teamRepulsion * deltaTime / dist);
 				}
 
+				//check if bee has not targeted another bee or a resource
 				if (bee.enemyTarget == null && bee.resourceTarget == null) {
+					//If number is lower than the aggression rate, then we find a target
 					if (Random.value < aggression) {
+						//find the opposite team
 						List<Bee> enemyTeam = teamsOfBees[1 - bee.team];
 						if (enemyTeam.Count > 0) {
 							bee.enemyTarget = enemyTeam[Random.Range(0,enemyTeam.Count)];
 						}
+					//If we don't target another bee we try to target a resource instead
 					} else {
 						bee.resourceTarget = ResourceManager.TryGetRandomResource();
 					}
+				//Check if we have targeted a bee
 				} else if (bee.enemyTarget != null) {
+					//If the targeted bee is dead we reset our target
 					if (bee.enemyTarget.dead) {
 						bee.enemyTarget = null;
 					} else {
+						//Calculate distance between current bee and the targeted bee
 						delta = bee.enemyTarget.position - bee.position;
 						float sqrDist = delta.x * delta.x + delta.y * delta.y + delta.z * delta.z;
+						//if the distance is longer than the total attack range the current bee moves closer to the target
 						if (sqrDist > attackDistance * attackDistance) {
 							bee.velocity += delta * (chaseForce * deltaTime / Mathf.Sqrt(sqrDist));
 						} else {
+							//If the distance is shorter than total attack range the bee attacks its target
 							bee.isAttacking = true;
 							bee.velocity += delta * (attackForce * deltaTime / Mathf.Sqrt(sqrDist));
+							//calculate if we hit the target bee
 							if (sqrDist < hitDistance * hitDistance) {
+								//If the targeted bee is hit is is killed and blood is spawned, the dead bee has its velocity cut in half
 								ParticleManager.SpawnParticle(bee.enemyTarget.position,ParticleType.Blood,bee.velocity * .35f,2f,6);
 								bee.enemyTarget.dead = true;
 								bee.enemyTarget.velocity *= .5f;
@@ -163,13 +191,19 @@ public class BeeManager : MonoBehaviour {
 							}
 						}
 					}
+				//The bee has a resource targeted
 				} else if (bee.resourceTarget != null) {
+					//get targeted resource
 					Resource resource = bee.resourceTarget;
+					//Check if the resource currently isn't being held by another bee
 					if (resource.holder == null) {
+						//If resource is dead we clear the target
 						if (resource.dead) {
 							bee.resourceTarget = null;
+						//If the targeted resource isn't at the top of the stack we clear the target
 						} else if (resource.stacked && ResourceManager.IsTopOfStack(resource) == false) {
 							bee.resourceTarget = null;
+						//If the resource is at the top of the stack the bee moves towards it and grabs the resource when it is close enough
 						} else {
 							delta = resource.position - bee.position;
 							float sqrDist = delta.x * delta.x + delta.y * delta.y + delta.z * delta.z;
@@ -179,6 +213,7 @@ public class BeeManager : MonoBehaviour {
 								ResourceManager.GrabResource(bee,resource);
 							}
 						}
+					//If the current bee is holding the resource it flies towards its own side and dropsthe resource there
 					} else if (resource.holder == bee) {
 						Vector3 targetPos = new Vector3(-Field.size.x * .45f + Field.size.x * .9f * bee.team,0f,bee.position.z);
 						delta = targetPos - bee.position;
@@ -190,12 +225,15 @@ public class BeeManager : MonoBehaviour {
 						} else {
 							bee.isHoldingResource = true;
 						}
+					//If the resource is held by a bee on the other team, that bee is set at a target to attack
 					} else if (resource.holder.team != bee.team) {
 						bee.enemyTarget = resource.holder;
+					//If the resource is held by a bee on own team the target is cleared
 					} else if (resource.holder.team == bee.team) {
 						bee.resourceTarget = null;
 					}
 				}
+			//If the bee is dead blood will spawn from it until its death-timer has run out, then it gets deleted
 			} else {
 				if (Random.value<(bee.deathTimer-.5f)*.5f) {
 					ParticleManager.SpawnParticle(bee.position,ParticleType.Blood,Vector3.zero);
@@ -207,9 +245,10 @@ public class BeeManager : MonoBehaviour {
 					DeleteBee(bee);
 				}
 			}
+			//Update position of the bee in regards to dealtaTime and the velocity it has
 			bee.position += deltaTime * bee.velocity;
 
-			
+			//Checks to keep the bees within the arena
 			if (System.Math.Abs(bee.position.x) > Field.size.x * .5f) {
 				bee.position.x = (Field.size.x * .5f) * Mathf.Sign(bee.position.x);
 				bee.velocity.x *= -.5f;
@@ -244,6 +283,7 @@ public class BeeManager : MonoBehaviour {
 		}
 	}
 	private void Update() {
+		//Loops through each bee, changing it's size, rotation and color if it is dead
 		for (int i=0;i<bees.Count;i++) {
 			float size = bees[i].size;
 			Vector3 scale = new Vector3(size,size,size);
@@ -257,6 +297,7 @@ public class BeeManager : MonoBehaviour {
 			if (bees[i].smoothDirection != Vector3.zero) {
 				rotation=Quaternion.LookRotation(bees[i].smoothDirection);
 			}
+			//Fades the color of the bee a bit if it is dead
 			Color color= teamColors[bees[i].team];
 			if (bees[i].dead) {
 				color *= .75f;
@@ -265,6 +306,7 @@ public class BeeManager : MonoBehaviour {
 			beeMatrices[i/beesPerBatch][i%beesPerBatch] = Matrix4x4.TRS(bees[i].position,rotation,scale);
 			beeColors[i/beesPerBatch][i%beesPerBatch] = color;
 		}
+		//Draws the mesh of each bee
 		for (int i = 0; i <= activeBatch; i++) {
 			if (beeMatrices[i].Count > 0) {
 				matProps.SetVectorArray("_Color",beeColors[i]);
--- Ported/Assets/Scripts/OldScripts/ResourceManager.cs
+++ Ported/Assets/Scripts/OldScripts/ResourceManager.cs
@@ -25,6 +25,7 @@ public class ResourceManager : MonoBehaviour {
 
 	public static ResourceManager instance;
 
+	//Tries to get a random resource, if the resorce is not at the top of the stack it is in, it can't be targeted by a bee
 	public static Resource TryGetRandomResource() {
 		if (instance.resources.Count==0) {
 			return null;
@@ -39,15 +40,18 @@ public class ResourceManager : MonoBehaviour {
 		}
 	}
 
+	//Checks if the resource is at the top of it's current stack
 	public static bool IsTopOfStack(Resource resource) {
 		int stackHeight = instance.stackHeights[resource.gridX,resource.gridY];
 		return resource.stackIndex == stackHeight - 1;
 	}
 
+	//gets its position on the stack
 	Vector3 GetStackPos(int x, int y, int height) {
 		return new Vector3(minGridPos.x+x*gridSize.x,-Field.size.y*.5f+(height+.5f)*resourceSize,minGridPos.y+y*gridSize.y);
 	}
 
+	//Snaps the resources together so its a solid stack that doesn't fall over
 	Vector3 NearestSnappedPos(Vector3 pos) {
 		int x, y;
 		GetGridIndex(pos,out x,out y);
@@ -61,22 +65,26 @@ public class ResourceManager : MonoBehaviour {
 		gridY = Mathf.Clamp(gridY,0,gridCounts.y - 1);
 	}
 
+	//Spawn a resource with a random position
 	void SpawnResource() {
 		Vector3 pos = new Vector3(minGridPos.x * .25f + Random.value * Field.size.x * .25f,Random.value * 10f,minGridPos.y + Random.value * Field.size.z);
 		SpawnResource(pos);
 	}
+	//spawn a resource with a known position
 	void SpawnResource(Vector3 pos) {
 		Resource resource = new Resource(pos);
 
 		resources.Add(resource);
 		matrices.Add(Matrix4x4.identity);
 	}
+	//Delete a specific resource
 	void DeleteResource(Resource resource) {
 		resource.dead = true;
 		resources.Remove(resource);
 		matrices.RemoveAt(matrices.Count - 1);
 	}
 
+	//Allows a bee to grab the resource
 	public static void GrabResource(Bee bee, Resource resource) {
 		resource.holder = bee;
 		resource.stacked = false;
@@ -86,6 +94,8 @@ public class ResourceManager : MonoBehaviour {
 	void Awake() {
 		instance = this;
 	}
+
+	//Initial setup of the manager
 	void Start () {
 		resources = new List<Resource>();
 		matrices = new List<Matrix4x4>();
@@ -95,12 +105,14 @@ public class ResourceManager : MonoBehaviour {
 		minGridPos = new Vector2((gridCounts.x-1f)*-.5f*gridSize.x,(gridCounts.y-1f)*-.5f*gridSize.y);
 		stackHeights = new int[gridCounts.x,gridCounts.y];
 
+		//Spawns the initial resources at random positions
 		for (int i=0;i<startResourceCount;i++) {
 			SpawnResource();
 		}
 	}
 
 	void Update() {
+		//spawn a resource on the mouse if there is less tha 1000 resources (thats a lot of fucking resources dawg)
 		if (resources.Count < 1000 && MouseRaycaster.isMouseTouchingField) {
 			if (Input.GetKey(KeyCode.Mouse0)) {
 				spawnTimer += Time.deltaTime;
@@ -111,16 +123,20 @@ public class ResourceManager : MonoBehaviour {
 			}
 		}
 
+		//loop through every single resource
 		for (int i=0;i<resources.Count;i++) {
 			Resource resource = resources[i];
 			if (resource.holder != null) {
+				//reset holder if holder is dead
 				if (resource.holder.dead) {
 					resource.holder = null;
+				//if holder is not dead set resource position and velocity to matching the holder (or atleast close to)
 				} else {
 					Vector3 targetPos = resource.holder.position - Vector3.up * (resourceSize + resource.holder.size)*.5f;
 					resource.position = Vector3.Lerp(resource.position,targetPos,carryStiffness * Time.deltaTime);
 					resource.velocity = resource.holder.velocity;
 				}
+			//If the resource is not stacked math is made to figure out where it is placed int the world, and if its moving or not
 			} else if (resource.stacked == false) {
 				resource.position = Vector3.Lerp(resource.position,NearestSnappedPos(resource.position),snapStiffness * Time.deltaTime);
 				resource.velocity.y += Field.gravity * Time.deltaTime;
@@ -135,6 +151,12 @@ public class ResourceManager : MonoBehaviour {
 						resource.velocity[(j + 2) % 3] *= .8f;
 					}
 				}
+				/*
+				 * This section is used for checking if a resource has been dropped within either side of the field
+				 * If it is within one of the side, and on the floor, new bees are spawned for the correct team, amount of bees spawned, and their location is decided form the resource values
+				 * A spawn flash particle is spawned when new bees are spawned
+				 * Lastly the resource is deleted
+				 */
 				if (resource.position.y < floorY) {
 					resource.position.y = floorY;
 					if (Mathf.Abs(resource.position.x) > Field.size.x * .4f) {
