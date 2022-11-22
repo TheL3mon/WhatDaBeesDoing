efee3d9d1025fbfd509ebd5ab74cf09823206bfb
paco <paco@itu.dk>
2022-11-22 11:00:34 +0100

    compiles now

--- Ported/Assets/Scripts/NewScripts/Data/ResourceData.cs
+++ Ported/Assets/Scripts/NewScripts/Data/ResourceData.cs
@@ -1,48 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 [GenerateAuthoringComponent]
 public struct ResourceData : IComponentData
 {
-    public int beesPerResource;
+ //   public int beesPerResource;
 
-	public Mesh resourceMesh;
-	public Material resourceMaterial;
-	public float resourceSize;
-	public float snapStiffness;
-	public float carryStiffness;
-	public float spawnRate; // 0.1f
-	[Space(10)]
-	public int startResourceCount;
+	//public Mesh resourceMesh;
+	//public Material resourceMaterial;
+	//public float resourceSize;
+	//public float snapStiffness;
+	//public float carryStiffness;
+	//public float spawnRate; // 0.1f
+	//[Space(10)]
+	//public int startResourceCount;
 
-	public List<Resource> resources;
-	public List<Matrix4x4> matrices;
-	public Vector2Int gridCounts;
-	public Vector2 gridSize;
-	public Vector2 minGridPos;
+	//public List<Resource> resources;
+	//public List<Matrix4x4> matrices;
+	//public int2 gridCounts;
+	//public float2 gridSize;
+	//public float2 minGridPos;
 
-	public int[,] stackHeights;
+	//public int[,] stackHeights;
 
-	float spawnTimer; // 0
+	//float spawnTimer; // 0
 
 	//public static ResourceManager instance;
 }
 
 public struct Resource : IComponentData
 {
-	public Vector3 position;
+	public float3 position;
 	public bool stacked;
 	public int stackIndex;
 	public int gridX;
 	public int gridY;
-	public Bee holder;
-	public Vector3 velocity;
+	//public Bee holder;
+	public float3 velocity;
 	public bool dead;
 
-	//public Resource(Vector3 myPosition)
-	//{
-	//	position = myPosition;
-	//}
+    //public Resource(Vector3 myPosition)
+    //{
+    //	position = myPosition;
+    //}
 }
