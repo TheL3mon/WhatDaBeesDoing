4aa1279c61e291e10208cf5c0670a90fe59d31d4
patrick2102 <48348311+patrick2102@users.noreply.github.com>
2022-12-29 11:50:19 +0100

    bee death stuff

--- Ported/Assets/Scripts/NewScripts/Systems/BeeDeathSystem.cs
+++ Ported/Assets/Scripts/NewScripts/Systems/BeeDeathSystem.cs
@@ -5,13 +5,16 @@ using Unity.Entities;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public partial class BeeDeathSystem : SystemBase
 {
     private EndSimulationEntityCommandBufferSystem _endSimulationEntityCommandBufferSystem;
 
     protected override void OnCreate()
-    {
+    {        var resourceQuery = GetEntityQuery(ComponentType.ReadOnly<ResourceTag>());
+        var resourceArr = resourceQuery.ToEntityArray(Allocator.Persistent);
         _endSimulationEntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
@@ -24,15 +27,24 @@ public partial class BeeDeathSystem : SystemBase
         var deadQuery = GetEntityQuery(ComponentType.ReadOnly<DeadTag>());
         var deadArr = deadQuery.ToEntityArray(Allocator.Persistent);
 
-        var beeStatus = GetComponentDataFromEntity<Bee>(true);
+        var resourceQuery = GetEntityQuery(ComponentType.ReadOnly<ResourceTag>());
+        var resourceArr = resourceQuery.ToEntityArray(Allocator.Persistent);
 
-        if(deadArr.Length > 0)
+        var beeStatus = GetComponentDataFromEntity<Bee>(true);
+        var resourceStatus = GetComponentDataFromEntity<Resource>(false);
+
+        var positions = GetComponentDataFromEntity<Translation>(false);
+
+        if (deadArr.Length > 0)
         {
 
             var deadBeeJob = new deadBeeJob
             {
                 ecb = ecb,
-                deadBees = deadArr
+                deadBees = deadArr,
+                resources = resourceArr,
+                resourceStatus = resourceStatus,
+                positions = positions
             }.Schedule();
 
             deadBeeJob.Complete();
@@ -61,17 +73,48 @@ public partial struct deadBeeJob : IJobEntity
 
     public EntityCommandBuffer ecb;
     public NativeArray<Entity> deadBees;
+    public NativeArray<Entity> resources;
+    public ComponentDataFromEntity<Resource> resourceStatus;
+    public ComponentDataFromEntity<Translation> positions;
 
     void Execute(Entity e, ref Bee bee)
     {
-        if(deadBees.Contains(e))
+
+        var targetResourceIndex = resources.IndexOf(bee.resourceTarget);
+
+        if (targetResourceIndex != -1)
+        {
+            var resource = resources[targetResourceIndex];
+            var status = resourceStatus[resource];
+
+            var holder = status.holder;
+
+            if (holder == e)
+            {
+                Debug.Log("Resource holder dead.");
+                var r = new Resource();
+                r.position = positions[e].Value;
+                r.height = -1;
+                r.holderTeam = -1;
+
+                var fallingResourceTag = new FallingResourceTag();
+
+                ecb.SetComponent(resource, r);
+                ecb.AddComponent(resource, fallingResourceTag);
+            }
+
+        }
+
+        if (deadBees.Contains(e))
         {
             bee.dead = true;
         }
-        if(deadBees.Contains(bee.enemyTarget))
+
+        if (deadBees.Contains(bee.enemyTarget))
         {
             bee.enemyTarget = Entity.Null;
         }
+
     }
 
 }
